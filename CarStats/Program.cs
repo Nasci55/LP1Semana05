28d7239aee1152c6fb12fd604928af44b1632cb3
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-21 17:17:43 +0000

    Add new Project

---
+++ CarStats/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Spectre.Console;
+using Spectre.Console;
+using Bogus;
+using Bogus.DataSets;
+
+namespace CarStats
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Randomizer.Seed = new Random(int.Parse(args[0]));
+            Faker faker = new Faker();
+
+            BarChart bc = new BarChart();
+
+            float number = new Bogus.Randomizer(1, 20);
+        }
+    }
+}
